72e4d01a0682757288cc4813a0b8ba56422cca72
Jon Calalang <jon.calalang@gmail.com>
2022-10-06 16:40:28 -0700

    remove the string format

--- distributed-cloud/variables.tf
+++ distributed-cloud/variables.tf
@@ -3,11 +3,9 @@
 # F5XC Environment
 
 variable "namespace" {
-  type    = list(string)
   default = ["j-calalang"]
 }
 
 variable "owner" {
-  type    = list(string)
   default = ["j.calalang@f5.com"]
 }
