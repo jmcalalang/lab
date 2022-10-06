96336d254850bb1fb0968b57633249b6a50d21f1
Jon Calalang <jon.calalang@gmail.com>
2022-10-06 15:42:01 -0700

    create a pool

---
+++ distributed-cloud/f5Pool.tf
@@ -0,0 +1,24 @@
+resource "volterra_origin_pool" "f5Pool" {
+  name                   = "f5Pool"
+  namespace              = "[var.namespace]"
+  endpoint_selection     = ["DISTRIBUTED"]
+  loadbalancer_algorithm = ["ROUND_ROBIN"]
+
+  origin_servers {
+    // One of the arguments from this list "custom_endpoint_object private_name k8s_service private_ip consul_service vn_private_ip vn_private_name public_ip public_name" must be set
+
+    public_name {
+      dns_name = "f5.com"
+    }
+
+    labels = {
+      "owner" = "[var.owner]"
+    }
+  }
+
+  // One of the arguments from this list "port automatic_port" must be set
+  port = "443"
+
+  // One of the arguments from this list "no_tls use_tls" must be set
+  no_tls = true
+}
