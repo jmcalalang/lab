9f943658306ca5fd05b6c248af6c34b0f6ca28fd
Jon Calalang <jon.calalang@gmail.com>
2022-10-06 15:32:22 -0700

    missed the provider

--- distributed-cloud/main.tf
+++ distributed-cloud/main.tf
@@ -1,8 +1,17 @@
 # Main
 
 # F5XC Provider
-provider "volterra" {
-  api_p12_file     = "../certs/f5-sa.console.ves.volterra.io.api-creds.p12"
-  url              = "https://f5-sa.console.ves.volterra.io/api"
+
+terraform {
+  required_providers {
+    volterra = {
+      source  = "volterraedge/volterra"
+      version = "0.11.14"
+    }
+  }
 }
 
+provider "volterra" {
+  api_p12_file = "../certs/f5-sa.console.ves.volterra.io.api-creds.p12"
+  url          = "https://f5-sa.console.ves.volterra.io/api"
+}
