9646a311cbafbf50e28b414b7f591f1dcf6db4b4
Jon Calalang <jon.calalang@gmail.com>
2022-10-08 00:19:54 -0700

    yes i did

--- distributed-cloud/main.tf
+++ distributed-cloud/main.tf
@@ -8,7 +8,7 @@ terraform {
   }
   required_providers {
     volterra = {
-      source  = "volterraedge/terraform-provider-volterra"
+      source  = "volterraedge/volterra"
       version = "0.11.14"
     }
   }
