b4274647f0e788fb34fb95a39a662e2ef5673881
Jon Calalang <jon.calalang@gmail.com>
2022-10-10 16:12:03 -0700

    adding threat campains

--- distributed-cloud/app-firewall-default.tf
+++ distributed-cloud/app-firewall-default.tf
@@ -1,19 +1,25 @@
 resource "volterra_app_firewall" "example" {
   name      = "application-firewall-defualt"
   namespace = var.namespace
-
-  monitoring = true
-
-  allow_all_response_codes = true
-
-  default_anonymization = true
-
-  enable_threat_campaigns = true
-
+  labels = {
+    "owner" = var.owner
+  }
   blocking_page {
     response_code = 403
     blocking_page = "string:///PGh0bWw+PGhlYWQ+PHRpdGxlPkNhbGFsYW5nIFNpdGUgUmVqZWN0ZWQ8L3RpdGxlPjwvaGVhZD48Ym9keT5UaGUgcmVxdWVzdGVkIFVSTCB3YXMgcmVqZWN0ZWQuIFBsZWFzZSBjb25zdWx0IHdpdGggeW91ciBhZG1pbmlzdHJhdG9yLjxici8+PGJyLz5Zb3VyIHN1cHBvcnQgSUQgaXM6IHt7cmVxdWVzdF9pZH19PGJyLz48YnIvPjxhIGhyZWY9ImphdmFzY3JpcHQ6aGlzdG9yeS5iYWNrKCkiPltHbyBCYWNrXTwvYT48L2JvZHk+PC9odG1sPg=="
   }
+  detection_settings {
+    signature_selection_setting = {
+      default_attack_type_settings    = true
+      high_medium_accuracy_signatures = true
+    }
+    enable_threat_campaigns    = true
+    enable_suppression         = true
+    default_violation_settings = true
+  }
+  monitoring                 = true
+  allow_all_response_codes   = true
+  default_anonymization      = true
   default_bot_setting        = true
   default_detection_settings = true
   blocking                   = true
