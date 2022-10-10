9dc50af67bff49862fe0f645a771789fc86783eb
Jon Calalang <jon.calalang@gmail.com>
2022-10-10 15:19:12 -0700

    try again

--- distributed-cloud/app-firewall-default.tf
+++ distributed-cloud/app-firewall-default.tf
@@ -12,19 +12,10 @@ resource "volterra_app_firewall" "example" {
 
   // One of the arguments from this list "use_default_blocking_page blocking_page" must be set
   blocking_page {
-    response_code = OK
+    response_code = [OK]
     blocking_page = "PGh0bWw+PGhlYWQ+PHRpdGxlPkNhbGFsYW5nIFNpdGUgUmVqZWN0ZWQ8L3RpdGxlPjwvaGVhZD48Ym9keT5UaGUgcmVxdWVzdGVkIFVSTCB3YXMgcmVqZWN0ZWQuIFBsZWFzZSBjb25zdWx0IHdpdGggeW91ciBhZG1pbmlzdHJhdG9yLjxici8+PGJyLz5Zb3VyIHN1cHBvcnQgSUQgaXM6IHt7cmVxdWVzdF9pZH19PGJyLz48YnIvPjxhIGhyZWY9ImphdmFzY3JpcHQ6aGlzdG9yeS5iYWNrKCkiPltHbyBCYWNrXTwvYT48L2JvZHk+PC9odG1sPg=="
   }
-  // One of the arguments from this list "default_bot_setting bot_protection_setting" must be set
-  bot_protection_setting {
-    malicious_bot_action  = BLOCK
-    suspicious_bot_action = REPORT
-    good_bot_action       = REPORT
-  }
-
-  // One of the arguments from this list "default_detection_settings detection_settings" must be set
+  default_bot_setting        = true
   default_detection_settings = true
-
-  // One of the arguments from this list "use_loadbalancer_setting blocking monitoring" must be set
-  blocking = true
+  blocking                   = true
 }
