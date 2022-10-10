4398a7ff6212d41c22dde68a9bc4612a830ad5e4
Jon Calalang <jon.calalang@gmail.com>
2022-10-10 15:33:49 -0700

    wrong spot

--- distributed-cloud/app-firewall-default.tf
+++ distributed-cloud/app-firewall-default.tf
@@ -9,13 +9,12 @@ resource "volterra_app_firewall" "example" {
 
   // One of the arguments from this list "disable_anonymization default_anonymization custom_anonymization" must be set
   default_anonymization = true
-
+  enable_threat_campaigns    = true
   // One of the arguments from this list "use_default_blocking_page blocking_page" must be set
   blocking_page {
     response_code = 403
     blocking_page = "string:///PGh0bWw+PGhlYWQ+PHRpdGxlPkNhbGFsYW5nIFNpdGUgUmVqZWN0ZWQ8L3RpdGxlPjwvaGVhZD48Ym9keT5UaGUgcmVxdWVzdGVkIFVSTCB3YXMgcmVqZWN0ZWQuIFBsZWFzZSBjb25zdWx0IHdpdGggeW91ciBhZG1pbmlzdHJhdG9yLjxici8+PGJyLz5Zb3VyIHN1cHBvcnQgSUQgaXM6IHt7cmVxdWVzdF9pZH19PGJyLz48YnIvPjxhIGhyZWY9ImphdmFzY3JpcHQ6aGlzdG9yeS5iYWNrKCkiPltHbyBCYWNrXTwvYT48L2JvZHk+PC9odG1sPg=="
   }
-  enable_threat_campaigns    = true
   default_bot_setting        = true
   default_detection_settings = true
   blocking                   = true
