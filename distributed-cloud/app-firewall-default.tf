5bfc0961c47cc13be66de464028922aef36577df
Jon Calalang <jon.calalang@gmail.com>
2022-10-10 16:28:59 -0700

    no =?

--- distributed-cloud/app-firewall-default.tf
+++ distributed-cloud/app-firewall-default.tf
@@ -9,7 +9,7 @@ resource "volterra_app_firewall" "example" {
     blocking_page = "string:///PGh0bWw+PGhlYWQ+PHRpdGxlPkNhbGFsYW5nIFNpdGUgUmVqZWN0ZWQ8L3RpdGxlPjwvaGVhZD48Ym9keT5UaGUgcmVxdWVzdGVkIFVSTCB3YXMgcmVqZWN0ZWQuIFBsZWFzZSBjb25zdWx0IHdpdGggeW91ciBhZG1pbmlzdHJhdG9yLjxici8+PGJyLz5Zb3VyIHN1cHBvcnQgSUQgaXM6IHt7cmVxdWVzdF9pZH19PGJyLz48YnIvPjxhIGhyZWY9ImphdmFzY3JpcHQ6aGlzdG9yeS5iYWNrKCkiPltHbyBCYWNrXTwvYT48L2JvZHk+PC9odG1sPg=="
   }
   detection_settings {
-    signature_selection_setting = {
+    signature_selection_setting {
       default_attack_type_settings    = true
       high_medium_accuracy_signatures = true
     }
