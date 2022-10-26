b7b5b1c56b3a90c888429512553d3d7632b03a3b
Jon Calalang <jon.calalang@gmail.com>
2022-10-26 08:49:21 -0700

    move to just godaddy?

--- distributed-cloud/http-lb-nginx-calalang-net.tf
+++ distributed-cloud/http-lb-nginx-calalang-net.tf
@@ -45,18 +45,18 @@ resource "volterra_http_loadbalancer" "http-lb-nginx-calalang-net" {
   add_location                     = true
 }
 
-# HTTP Load Balancer for NGINX Servers Outputs
-
-output "cname_challenge" {
-  description = "Acme Challenge for domain"
-  value       = volterra_http_loadbalancer.http-lb-nginx-calalang-net.auto_cert_info.dns_records.value.id
-}
-
-output "cname" {
-  description = "Redirect to F5XC"
-  value       = volterra_http_loadbalancer.http-lb-nginx-calalang-net.host_name.id
-}
-
+## HTTP Load Balancer for NGINX Servers Outputs
+#
+#output "cname_challenge" {
+#  description = "Acme Challenge for domain"
+#  value       = volterra_http_loadbalancer.http-lb-nginx-calalang-net.auto_cert_info.dns_records.value.id
+#}
+#
+#output "cname" {
+#  description = "Redirect to F5XC"
+#  value       = volterra_http_loadbalancer.http-lb-nginx-calalang-net.host_name.id
+#}
+#
 # GoDaddy F5XC Challenge and Redirect
 
 resource "godaddy_domain_record" "http-lb-nginx-calalang-net" {
@@ -65,14 +65,14 @@ resource "godaddy_domain_record" "http-lb-nginx-calalang-net" {
   record {
     name = "_acme-challenge.nginx"
     type = "CNAME"
-    data = cname_challenge.value
+    data = module.volterra_http_loadbalancer.http-lb-nginx-calalang-net.auto_cert_info["dns_records"].value.id
     ttl  = 3600
   }
 
   record {
     name = "nginx"
     type = "CNAME"
-    data = cname.value
+    data = module.volterra_http_loadbalancer.http-lb-nginx-calalang-net.host_name.id
     ttl  = 3600
   }
 }
