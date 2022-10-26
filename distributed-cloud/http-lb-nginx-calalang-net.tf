04025e575c61f8c33fcd87c9b0bbda7e178b996d
Jon Calalang <jon.calalang@gmail.com>
2022-10-26 08:52:10 -0700

    remove module

--- distributed-cloud/http-lb-nginx-calalang-net.tf
+++ distributed-cloud/http-lb-nginx-calalang-net.tf
@@ -65,14 +65,14 @@ resource "godaddy_domain_record" "http-lb-nginx-calalang-net" {
   record {
     name = "_acme-challenge.nginx"
     type = "CNAME"
-    data = module.volterra_http_loadbalancer.http-lb-nginx-calalang-net.auto_cert_info["dns_records"].value.id
+    data = volterra_http_loadbalancer.http-lb-nginx-calalang-net.auto_cert_info["dns_records"].value.id
     ttl  = 3600
   }
 
   record {
     name = "nginx"
     type = "CNAME"
-    data = module.volterra_http_loadbalancer.http-lb-nginx-calalang-net.host_name.id
+    data = volterra_http_loadbalancer.http-lb-nginx-calalang-net.host_name.id
     ttl  = 3600
   }
 }
