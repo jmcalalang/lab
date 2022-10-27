4bea429db501926bf6683fa5e2fa0203bca11a6a
Jon Calalang <jon.calalang@gmail.com>
2022-10-27 08:50:17 -0700

    no = on simple

--- distributed-cloud/http-lb-kubernetes-calalang-net.tf
+++ distributed-cloud/http-lb-kubernetes-calalang-net.tf
@@ -10,31 +10,33 @@ resource "volterra_http_loadbalancer" "http-lb-kubernetes-calalang-net" {
   domains                         = ["test-kubernetes.calalang.net", "test-argo.calalang.net"]
   advertise_on_public_default_vip = true
   round_robin                     = true
-  simple_route {
-    http_method = ANY
-    path = {
-      regex = ".*"
-    }
-    origin_pools = [
-      {
-        pool = {
-          namespace = j-calalang
-          name      = kubernetes-service-pool
-          kind      = origin_pool
+  routes {
+    simple_route {
+      http_method = ANY
+      path = {
+        regex = ".*"
+      }
+      origin_pools = [
+        {
+          pool = {
+            namespace = j-calalang
+            name      = kubernetes-service-pool
+            kind      = origin_pool
+          }
+          weight           = 1
+          priority         = 1
+          endpoint_subsets = {}
         }
-        weight           = 1
-        priority         = 1
-        endpoint_subsets = {}
-      }
-    ]
-    headers = [
-      {
-        name         = HOST
-        exact        = "test-kubernetes.calalang.net"
-        invert_match = false
-      }
-    ]
-    host_rewrite = "test-kubernetes.calalang.net"
+      ]
+      headers = [
+        {
+          name         = HOST
+          exact        = "test-kubernetes.calalang.net"
+          invert_match = false
+        }
+      ]
+      host_rewrite = "test-kubernetes.calalang.net"
+    }
   }
   https_auto_cert {
     add_hsts              = true
