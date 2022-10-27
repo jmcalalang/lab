e345cdb7ef3f39da29054a6b5f923f432fdefd90
Jon Calalang <jon.calalang@gmail.com>
2022-10-27 08:48:07 -0700

    just simple route?

--- distributed-cloud/http-lb-kubernetes-calalang-net.tf
+++ distributed-cloud/http-lb-kubernetes-calalang-net.tf
@@ -10,33 +10,31 @@ resource "volterra_http_loadbalancer" "http-lb-kubernetes-calalang-net" {
   domains                         = ["test-kubernetes.calalang.net", "test-argo.calalang.net"]
   advertise_on_public_default_vip = true
   round_robin                     = true
-  routes {
-    simple_route = {
-      http_method = ANY
-      path = {
-        regex = ".*"
-      }
-      origin_pools = [
-        {
-          pool = {
-            namespace = j-calalang
-            name      = kubernetes-service-pool
-            kind      = origin_pool
-          }
-          weight           = 1
-          priority         = 1
-          endpoint_subsets = {}
-        }
-      ]
-      headers = [
-        {
-          name         = HOST
-          exact        = "test-kubernetes.calalang.net"
-          invert_match = false
-        }
-      ]
-      host_rewrite = "test-kubernetes.calalang.net"
+  simple_route {
+    http_method = ANY
+    path = {
+      regex = ".*"
     }
+    origin_pools = [
+      {
+        pool = {
+          namespace = j-calalang
+          name      = kubernetes-service-pool
+          kind      = origin_pool
+        }
+        weight           = 1
+        priority         = 1
+        endpoint_subsets = {}
+      }
+    ]
+    headers = [
+      {
+        name         = HOST
+        exact        = "test-kubernetes.calalang.net"
+        invert_match = false
+      }
+    ]
+    host_rewrite = "test-kubernetes.calalang.net"
   }
   https_auto_cert {
     add_hsts              = true
