35158b9c700be870ac20035d04e83fac29192827
Jon Calalang <jon.calalang@gmail.com>
2022-10-28 10:30:35 -0700

    terraform fmt

--- distributed-cloud/f5xc-sites.tf
+++ distributed-cloud/f5xc-sites.tf
@@ -1,8 +1,8 @@
 resource "volterra_registration_approval" "calalang-aks-cluster" {
-  cluster_name  = "calalang-aks-cluster"
-  cluster_size  = 1
-  retry = 5
-  wait_time = 60
-  latitude = 44.09133
-  longitude = -121.31144
+  cluster_name = "calalang-aks-cluster"
+  cluster_size = 1
+  retry        = 5
+  wait_time    = 60
+  latitude     = 44.09133
+  longitude    = -121.31144
 }
