c2d8df2cfcddbac679bebe59eee1891f4a588d27
Jon Calalang <jon.calalang@gmail.com>
2022-11-04 09:16:19 -0700

    adding endpoint for jwt auth

--- distributed-cloud/http-lb-kubernetes-calalang-net.tf
+++ distributed-cloud/http-lb-kubernetes-calalang-net.tf
@@ -7,7 +7,7 @@ resource "volterra_http_loadbalancer" "http-lb-kubernetes-calalang-net" {
     "owner" = var.owner
   }
   description                     = "Global HTTPS Load Balancer for kubernetes.calalang.net"
-  domains                         = ["kubernetes.calalang.net", "argo.calalang.net", "nms.calalang.net"]
+  domains                         = ["kubernetes.calalang.net", "argo.calalang.net", "nms.calalang.net", "jwt.nms.calalang.net"]
   advertise_on_public_default_vip = true
   round_robin                     = true
   routes {
@@ -77,6 +77,29 @@ resource "volterra_http_loadbalancer" "http-lb-kubernetes-calalang-net" {
         invert_match = false
       }
       host_rewrite = "nms.calalang.net"
+    }
+  }
+  routes {
+    simple_route {
+      http_method = "ANY"
+      path {
+        regex = ".*"
+      }
+      origin_pools {
+        pool {
+          namespace = var.namespace
+          name      = volterra_origin_pool.pool-svc-nginx-ingress.name
+        }
+        weight           = 1
+        priority         = 1
+        endpoint_subsets = {}
+      }
+      headers {
+        name         = "HOST"
+        exact        = "jwt.nms.calalang.net"
+        invert_match = false
+      }
+      host_rewrite = "jwt.nms.calalang.net"
     }
   }
   https_auto_cert {
