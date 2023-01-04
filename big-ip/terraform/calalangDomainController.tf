380345de00a8bc8ca9009fa3d216bcc35a6c0ef8
Jon Calalang <jon.calalang@gmail.com>
2023-01-03 22:36:26 -0800

    terraform fmt

--- big-ip/terraform/calalangDomainController.tf
+++ big-ip/terraform/calalangDomainController.tf
@@ -1,5 +1,5 @@
-## bigip_fast_application This resource will create and manage FAST applications on BIG-IP from provided JSON declaration.
+## This resource will create and manage FAST applications on BIG-IP from provided JSON declaration.
 
 resource "bigip_fast_application" "calalangDomainController" {
-  fast_json = "${file("calalangDomainController.fast")}"
+  fast_json = file("calalangDomainController.fast")
 }
