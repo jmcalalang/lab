24b11f86330d528ae8cd293692624106ee8e8f82
Jon Calalang <jon.calalang@gmail.com>
2023-01-04 08:09:52 -0800

    playing with naming conventions

--- big-ip/terraform/fastApplications.tf
+++ big-ip/terraform/fastApplications.tf
@@ -1,6 +1,6 @@
 ## This resource will create and manage FAST applications on BIG-IP from provided JSON declaration.
 
-resource "bigip_fast_application" "calalangDomainController" {
+resource "bigip_fast_application" "ldap_10_0_2_15_fast" {
   template  = "bigip-fast-templates/ldap"
-  fast_json = ("${path.module}/applications/app_calalangDomainController.fast")
+  fast_json = ("${path.module}/applications/ldap_10_0_2_15_fast.json")
 }
