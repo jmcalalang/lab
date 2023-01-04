ab3c32b75e4a1e5883c8775af03b044623435695
Jon Calalang <jon.calalang@gmail.com>
2023-01-03 22:32:09 -0800

    add fast template to big-ip tf

---
+++ big-ip/terraform/calalangDomainController.tf
@@ -0,0 +1,5 @@
+## bigip_fast_application This resource will create and manage FAST applications on BIG-IP from provided JSON declaration.
+
+resource "bigip_fast_application" "calalangDomainController" {
+  fast_json = "${file("calalangDomainController.fast")}"
+}
