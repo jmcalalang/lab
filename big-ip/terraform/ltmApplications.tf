70eb8f585acb455555b4ef2e89b185e23f79bbf2
Jon Calalang <jon.calalang@gmail.com>
2023-01-17 11:33:44 -0800

    no array?

--- big-ip/terraform/ltmApplications.tf
+++ big-ip/terraform/ltmApplications.tf
@@ -6,8 +6,8 @@ resource "bigip_ltm_virtual_server" "https-10-0-2-51-terraform" {
   port                       = 443
   client_profiles            = ["/Common/clientssl"]
   source_address_translation = "automap"
-  irules { name = [bigip_ltm_irule.forward-nginxCalalangNet-vs.name] }
-  vlans         = ["/Common/external"]
-  vlans_enabled = "true"
-  profiles      = ["/Common/f5-tcp-progressive", "/Common/http"]
+  irules                     = [bigip_ltm_irule.forward-nginxCalalangNet-vs.name]
+  vlans                      = ["/Common/external"]
+  vlans_enabled              = "true"
+  profiles                   = ["/Common/f5-tcp-progressive", "/Common/http"]
 }
