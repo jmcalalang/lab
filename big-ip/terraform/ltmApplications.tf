7e12855d3544f8d8849fb90204ca586b7beed755
Jon Calalang <jon.calalang@gmail.com>
2023-01-17 11:39:27 -0800

    removing irules to clean state

--- big-ip/terraform/ltmApplications.tf
+++ big-ip/terraform/ltmApplications.tf
@@ -6,7 +6,6 @@ resource "bigip_ltm_virtual_server" "https-10-0-2-51-terraform" {
   port                       = 443
   client_profiles            = ["/Common/clientssl"]
   source_address_translation = "automap"
-  irules                     = [bigip_ltm_irule.forward-nginxCalalangNet-vs.name]
   vlans                      = ["/Common/external"]
   vlans_enabled              = "true"
   profiles                   = ["/Common/f5-tcp-progressive", "/Common/http"]
