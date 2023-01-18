002a89dd41d487b33c8bb8173d3becc1b6b39014
Jon Calalang <jon.calalang@gmail.com>
2023-01-17 18:06:16 -0800

    all kinds of breaking

--- big-ip/terraform/irules.tf
+++ big-ip/terraform/irules.tf
@@ -1,5 +1,5 @@
 # Loading from a file is the preferred method
-resource "bigip_ltm_irule" "forward-nginxCalalangNet-vs" {
-  name  = "/Common/forward-nginxCalalangNet-vs"
-  irule = file("${path.module}/irules/forward-nginxCalalangNet-vs.irule")
+resource "bigip_ltm_irule" "forward-nginx-org" {
+  name  = "/Common/forward-nginx-org"
+  irule = file("${path.module}/irules/forward-nginx-org.irule")
 }
