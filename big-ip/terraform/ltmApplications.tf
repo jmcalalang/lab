23a97dd7fcde76344a6112f55b8620b0cc2d0aab
Jon Calalang <jon.calalang@gmail.com>
2023-02-07 09:57:29 -0800

    remove oidc and rename pools

--- big-ip/terraform/ltmApplications.tf
+++ big-ip/terraform/ltmApplications.tf
@@ -9,14 +9,14 @@ resource "bigip_ltm_virtual_server" "https-10-0-2-12-terraform" {
   vlans                      = ["/Common/external"]
   vlans_enabled              = "true"
   profiles                   = ["/Common/f5-tcp-progressive", "/Common/http"]
-  pool                       = bigip_ltm_pool.pool-nginx-azure-instances-terraform.name
+  pool                       = bigip_ltm_pool.pool-ip-nginx-azure-instances-terraform.name
 }
 resource "bigip_ltm_monitor" "monitor-nginx-azure-instances-terraform" {
   name   = "/Common/monitor-nginx-azure-instances-terraform"
   parent = "/Common/http"
 }
-resource "bigip_ltm_pool" "pool-nginx-azure-instances-terraform" {
-  name                   = "/Common/pool-nginx-azure-instances-terraform"
+resource "bigip_ltm_pool" "pool-ip-nginx-azure-instances-terraform" {
+  name                   = "/Common/pool-ip-nginx-azure-instances-terraform"
   load_balancing_mode    = "round-robin"
   minimum_active_members = 1
   monitors               = [bigip_ltm_monitor.monitor-nginx-azure-instances-terraform.name]
@@ -25,7 +25,7 @@ resource "bigip_ltm_pool" "pool-nginx-azure-instances-terraform" {
 }
 resource "bigip_ltm_pool_attachment" "attach_node" {
   for_each = toset([bigip_ltm_node.node-10-0-3-5-terraform.name])
-  pool     = bigip_ltm_pool.pool-nginx-azure-instances-terraform.name
+  pool     = bigip_ltm_pool.pool-ip-nginx-azure-instances-terraform.name
   node     = "${each.key}:80"
 }
 resource "bigip_ltm_node" "node-10-0-3-5-terraform" {
