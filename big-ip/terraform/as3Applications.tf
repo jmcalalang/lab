deb96705ca263de0e96981edb59dba6c3053224a
Jon Calalang <jon.calalang@gmail.com>
2023-02-13 17:19:11 -0800

    re-create big-ip terraform, remove do ansible

--- big-ip/terraform/as3Applications.tf
+++ big-ip/terraform/as3Applications.tf
@@ -1,10 +1,10 @@
 # This resource will create and manage AS3 applications on BIG-IP from provided JSON declarations.
 
+### nginx application http + wip ###
 # http nginx.calalang.net
 resource "bigip_as3" "https-10-0-2-14-as3" {
   as3_json = file("${path.module}/applications/https-10-0-2-14-as3.json")
 }
-
 # wip nginx.calalang.net
 resource "bigip_as3" "wip-10-0-2-14-as3" {
   as3_json   = file("${path.module}/applications/wip-10-0-2-14-as3.json")
