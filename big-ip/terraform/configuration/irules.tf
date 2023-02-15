a677efc5c0c1cb95262c69a7b733a7b71001b8dd
Jon Calalang <jon.calalang@gmail.com>
2023-02-14 18:29:21 -0800

    kaboom

--- big-ip/terraform/configuration/irules.tf
+++
@@ -1,5 +0,0 @@
-# # Loading from a file is the preferred method
-# resource "bigip_ltm_irule" "forward-nginx-org" {
-#   name  = "/Common/forward-nginx-org"
-#   irule = file("${path.module}/irules/forward-nginx-org.irule")
-# }
