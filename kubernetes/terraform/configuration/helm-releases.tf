46405b48f90bff5e34607f34cea45267aaaa0888
Jon Calalang <jon.calalang@gmail.com>
2023-03-02 12:15:49 -0800

    bringing argo back

--- kubernetes/terraform/configuration/helm-releases.tf
+++ kubernetes/terraform/configuration/helm-releases.tf
@@ -14,18 +14,18 @@ resource "helm_release" "nginx" {
 
 }
 
-# resource "helm_release" "argocd" {
-#   name       = "argo-cd"
-#   repository = "https://github.com/argoproj/argo-helm/tree/main/charts/argo-cd"
-#   chart      = "argo-cd"
-# 
-#   values = [
-#     "${file("./files/helm/argocd-values.yaml")}"
-#   ]
-# 
-#   set {
-#     name  = "service.type"
-#     value = "ClusterIP"
-#   }
-# 
-# }
+ resource "helm_release" "argocd" {
+   name       = "argo"
+   repository = "https://argoproj.github.io/argo-helm"
+   chart      = "argo-cd"
+ 
+   values = [
+     "${file("./files/helm/argocd-values.yaml")}"
+   ]
+ 
+   set {
+     name  = "service.type"
+     value = "ClusterIP"
+   }
+ 
+ }
