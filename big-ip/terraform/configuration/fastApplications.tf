fd80704571dcf3b2015dd7ccd4650faeded7e67e
Jon Calalang <jon.calalang@gmail.com>
2023-03-02 18:07:45 -0800

    move fast to templates

--- big-ip/terraform/configuration/fastApplications.tf
+++ big-ip/terraform/configuration/fastApplications.tf
@@ -1,7 +1,13 @@
 # This resource will create and manage FAST applications on BIG-IP from provided JSON declarations.
 
-# LDAP Application
-resource "bigip_fast_application" "ldap-10-0-2-5-fast" {
-  template  = "bigip-fast-templates/ldap"
-  fast_json = file("${path.module}/applications/fast/ldap-10-0-2-5-fast.json")
+# FAST declarations
+resource "bigip_fast_application" "fast-declarations" {
+  for_each = fileset(path.module, "applications/fast/**.json")
+  fast_json = file("${path.module}/${each.key}")
 }
+
+## LDAP Application
+#resource "bigip_fast_application" "ldap-10-0-2-5-fast" {
+#  template  = "bigip-fast-templates/ldap"
+#  fast_json = file("${path.module}/applications/fast/ldap-10-0-2-5-fast.json")
+#}
