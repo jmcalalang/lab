1bf221e6bbd3286016a8afb51e472dbb48b5e6f0
Jon Calalang <jon.calalang@gmail.com>
2023-03-02 20:39:15 -0800

    back to state, clean up

--- big-ip/terraform/configuration/as3Applications.tf
+++ big-ip/terraform/configuration/as3Applications.tf
@@ -30,13 +30,3 @@ resource "bigip_as3" "udp-declarations" {
   for_each = fileset(path.module, "applications/as3/udp/**.json")
   as3_json = file("${path.module}/${each.key}")
 }
-
-## http bigip.calalang.net
-#resource "bigip_as3" "https-10-0-2-6-as3" {
-#  as3_json = file("${path.module}/applications/https-10-0-2-6-as3.json")
-#}
-## wip bigip.calalang.net
-#resource "bigip_as3" "wip-10-0-2-6-as3" {
-#  as3_json   = file("${path.module}/applications/wip-10-0-2-6-as3.json")
-#  depends_on = [bigip_as3.https-10-0-2-6-as3]
-#}
--- big-ip/terraform/configuration/fastApplications.tf
+++ big-ip/terraform/configuration/fastApplications.tf
@@ -1,11 +1,5 @@
 # This resource will create and manage FAST applications on BIG-IP from provided JSON declarations.
 
-## FAST declarations
-#resource "bigip_fast_application" "fast-declarations" {
-#  for_each  = fileset(path.module, "applications/fast/**.json")
-#  fast_json = file("${path.module}/${each.key}")
-#}
-
 # LDAP Application
 resource "bigip_fast_application" "ldap-10-0-2-5-fast" {
   template  = "bigip-fast-templates/ldap"
