cc2db40b03a6d6abc42b6c1c73a5f03a8105b9d2
Jon Calalang <jon.calalang@gmail.com>
2023-03-07 10:26:52 -0800

    namespace was in the wrong spot

--- kubernetes/terraform/configuration/helm-releases.tf
+++ kubernetes/terraform/configuration/helm-releases.tf
@@ -4,15 +4,15 @@ resource "helm_release" "argocd" {
   name       = "argo"
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo-cd"
+  namespace  = "argocd"
 
   values = [
     "${file("./files/helm/argocd-values.yaml")}"
   ]
 
   set {
-    name      = "service.type"
-    value     = "ClusterIP"
-    namespace = "argocd"
+    name  = "service.type"
+    value = "ClusterIP"
   }
 
 }
