ff7022031cb3630abde219fe559e311eff081ce1
Jon Calalang <jon.calalang@gmail.com>
2023-03-09 23:36:29 -0800

    changes for eod

--- kubernetes/terraform/infrastructure/roleAssignment.tf
+++ kubernetes/terraform/infrastructure/roleAssignment.tf
@@ -1,6 +1,22 @@
-resource "azurerm_role_assignment" "aks_role_assignment" {
+resource "azurerm_role_assignment" "AcrPull" {
   principal_id                     = azurerm_kubernetes_cluster.kubernetes_cluster[count.index].kubelet_identity[0].object_id
   role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.container_registry.id
+  skip_service_principal_aad_check = true
+  count                            = sum([var.aks-instance-count])
+}
+
+resource "azurerm_role_assignment" "subnetRead" {
+  principal_id                     = azurerm_kubernetes_cluster.kubernetes_cluster[count.index].kubelet_identity[0].object_id
+  role_definition_name             = "subnetRead"
+  scope                            = azurerm_container_registry.container_registry.id
+  skip_service_principal_aad_check = true
+  count                            = sum([var.aks-instance-count])
+}
+
+resource "azurerm_role_assignment" "subnetWrite" {
+  principal_id                     = azurerm_kubernetes_cluster.kubernetes_cluster[count.index].kubelet_identity[0].object_id
+  role_definition_name             = "subnetWrite"
   scope                            = azurerm_container_registry.container_registry.id
   skip_service_principal_aad_check = true
   count                            = sum([var.aks-instance-count])
