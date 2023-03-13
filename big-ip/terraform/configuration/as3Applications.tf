68a34e669a26f4a007e318d78e1d3707fab7bccf
Jon Calalang <jon.calalang@gmail.com>
2023-03-12 22:03:17 -0700

    as3 tf config fmt

--- big-ip/terraform/configuration/as3Applications.tf
+++ big-ip/terraform/configuration/as3Applications.tf
@@ -2,36 +2,36 @@
 
 # HTTP declarations
 resource "bigip_as3" "http-declarations" {
-  for_each = fileset(path.module, "applications/as3/http/**.json")
-  as3_json = file("${path.module}/${each.key}")
+  for_each        = fileset(path.module, "applications/as3/http/**.json")
+  as3_json        = file("${path.module}/${each.key}")
   ignore_metadata = true
 }
 
 # HTTPs declarations
 resource "bigip_as3" "https-declarations" {
-  for_each = fileset(path.module, "applications/as3/https/**.json")
-  as3_json = file("${path.module}/${each.key}")
+  for_each        = fileset(path.module, "applications/as3/https/**.json")
+  as3_json        = file("${path.module}/${each.key}")
   ignore_metadata = true
 }
 
 # WIP declarations
 resource "bigip_as3" "wip-declarations" {
-  for_each   = fileset(path.module, "applications/as3/wip/**.json")
-  as3_json   = file("${path.module}/${each.key}")
+  for_each        = fileset(path.module, "applications/as3/wip/**.json")
+  as3_json        = file("${path.module}/${each.key}")
   ignore_metadata = true
-  depends_on = [bigip_as3.http-declarations, bigip_as3.https-declarations]
+  depends_on      = [bigip_as3.http-declarations, bigip_as3.https-declarations]
 }
 
 # TCP declarations
 resource "bigip_as3" "tcp-declarations" {
-  for_each = fileset(path.module, "applications/as3/tcp/**.json")
-  as3_json = file("${path.module}/${each.key}")
+  for_each        = fileset(path.module, "applications/as3/tcp/**.json")
+  as3_json        = file("${path.module}/${each.key}")
   ignore_metadata = true
 }
 
 # UDP declarations
 resource "bigip_as3" "udp-declarations" {
-  for_each = fileset(path.module, "applications/as3/udp/**.json")
-  as3_json = file("${path.module}/${each.key}")
+  for_each        = fileset(path.module, "applications/as3/udp/**.json")
+  as3_json        = file("${path.module}/${each.key}")
   ignore_metadata = true
 }
