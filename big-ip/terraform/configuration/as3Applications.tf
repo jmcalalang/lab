2686e7455b3bb7be7c00ff27c298a182a71a8ae8
Jon Calalang <jon.calalang@gmail.com>
2023-03-12 21:54:39 -0700

    ignore as3 metadata, add argo applications

--- big-ip/terraform/configuration/as3Applications.tf
+++ big-ip/terraform/configuration/as3Applications.tf
@@ -4,18 +4,21 @@
 resource "bigip_as3" "http-declarations" {
   for_each = fileset(path.module, "applications/as3/http/**.json")
   as3_json = file("${path.module}/${each.key}")
+  ignore_metadata = true
 }
 
 # HTTPs declarations
 resource "bigip_as3" "https-declarations" {
   for_each = fileset(path.module, "applications/as3/https/**.json")
   as3_json = file("${path.module}/${each.key}")
+  ignore_metadata = true
 }
 
 # WIP declarations
 resource "bigip_as3" "wip-declarations" {
   for_each   = fileset(path.module, "applications/as3/wip/**.json")
   as3_json   = file("${path.module}/${each.key}")
+  ignore_metadata = true
   depends_on = [bigip_as3.http-declarations, bigip_as3.https-declarations]
 }
 
@@ -23,10 +26,12 @@ resource "bigip_as3" "wip-declarations" {
 resource "bigip_as3" "tcp-declarations" {
   for_each = fileset(path.module, "applications/as3/tcp/**.json")
   as3_json = file("${path.module}/${each.key}")
+  ignore_metadata = true
 }
 
 # UDP declarations
 resource "bigip_as3" "udp-declarations" {
   for_each = fileset(path.module, "applications/as3/udp/**.json")
   as3_json = file("${path.module}/${each.key}")
+  ignore_metadata = true
 }
