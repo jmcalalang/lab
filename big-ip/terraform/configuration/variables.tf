5ccd5e81705614a54d4ba0da894d7984c16c213e
Jon Calalang <jon.calalang@gmail.com>
2023-03-16 22:07:47 -0700

    missed variable definition

--- big-ip/terraform/configuration/variables.tf
+++ big-ip/terraform/configuration/variables.tf
@@ -4,3 +4,8 @@ variable "ad_service_ldap_password" {
   type        = string
   description = "Password for the LDAP Service Account"
 }
+
+variable "ARM_SUBSCRIPTION_ID" {
+  type        = string
+  description = "Azure subscription ID"
+}
