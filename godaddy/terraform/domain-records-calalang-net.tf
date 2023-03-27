c2e7d00feb0035e3db9985e3648f91461ad7d6b9
Jon Calalang <jon.calalang@gmail.com>
2023-03-27 14:36:28 -0700

    state and dns records

--- godaddy/terraform/domain-records-calalang-net.tf
+++ godaddy/terraform/domain-records-calalang-net.tf
@@ -6,56 +6,56 @@ resource "godaddy_domain_record" "domain-records-calalang-net" {
   record {
     name = "_acme-challenge.kubernetes"
     type = "CNAME"
-    data = "1874753db2e34be5a4087a1c68ac436a.autocerts.ves.volterra.io"
+    data = "8765fe94740a457f96ce4d46132963dd.autocerts.ves.volterra.io"
     ttl  = 3600
   }
 
   record {
     name = "kubernetes"
     type = "CNAME"
-    data = "ves-io-7e9a0cf1-ab2a-496f-bd45-4b1cdaaf258d.ac.vh.ves.io"
+    data = "ves-io-4d164d48-e42e-4886-82f1-ad381fb77397.ac.vh.ves.io"
     ttl  = 3600
   }
 
   record {
     name = "_acme-challenge.argo"
     type = "CNAME"
-    data = "16e96b012388475e984aeaaae866b195.autocerts.ves.volterra.io"
+    data = "4d628d8732d7409b887a4a5e33c71a91.autocerts.ves.volterra.io"
     ttl  = 3600
   }
 
   record {
     name = "argo"
     type = "CNAME"
-    data = "ves-io-7e9a0cf1-ab2a-496f-bd45-4b1cdaaf258d.ac.vh.ves.io"
+    data = "ves-io-4d164d48-e42e-4886-82f1-ad381fb77397.ac.vh.ves.io"
     ttl  = 3600
   }
 
   record {
     name = "_acme-challenge.nms"
     type = "CNAME"
-    data = "e71fe4ab75b743168f02da479973570e.autocerts.ves.volterra.io"
+    data = "e979678f91c74c5f83d909055bb970f7.autocerts.ves.volterra.io"
     ttl  = 3600
   }
 
   record {
     name = "nms"
     type = "CNAME"
-    data = "ves-io-7e9a0cf1-ab2a-496f-bd45-4b1cdaaf258d.ac.vh.ves.io"
+    data = "ves-io-4d164d48-e42e-4886-82f1-ad381fb77397.ac.vh.ves.io"
     ttl  = 3600
   }
 
   record {
     name = "_acme-challenge.jwt.nms"
     type = "CNAME"
-    data = "b33e7ccd5258423e8d6f4c8189c1ffaf.autocerts.ves.volterra.io"
+    data = "77fcf4a2b99f4acfa91a9923ce33a594.autocerts.ves.volterra.io"
     ttl  = 3600
   }
 
   record {
     name = "jwt.nms"
     type = "CNAME"
-    data = "ves-io-7e9a0cf1-ab2a-496f-bd45-4b1cdaaf258d.ac.vh.ves.io"
+    data = "ves-io-4d164d48-e42e-4886-82f1-ad381fb77397.ac.vh.ves.io"
     ttl  = 3600
   }
 
@@ -64,14 +64,14 @@ resource "godaddy_domain_record" "domain-records-calalang-net" {
   record {
     name = "_acme-challenge.nginx"
     type = "CNAME"
-    data = "c64a346eb873471d8c9a7c779e57914b.autocerts.ves.volterra.io"
+    data = "c5ba287a765b47a783df3e510a0d3043.autocerts.ves.volterra.io"
     ttl  = 3600
   }
 
   record {
     name = "nginx"
     type = "CNAME"
-    data = "ves-io-36af6783-92cf-415f-b2ac-f2ac5c41cbf8.ac.vh.ves.io"
+    data = "ves-io-6c9205f9-340d-4a50-8ec5-b517e2ea8fca.ac.vh.ves.io"
     ttl  = 3600
   }
 
@@ -80,42 +80,42 @@ resource "godaddy_domain_record" "domain-records-calalang-net" {
   record {
     name = "_acme-challenge.bigip"
     type = "CNAME"
-    data = "434db158db7e4a53a52111b26284c59c.autocerts.ves.volterra.io"
+    data = "3a038a23d2d94ab48964811d58ffbfeb.autocerts.ves.volterra.io"
     ttl  = 3600
   }
 
   record {
     name = "bigip"
     type = "CNAME"
-    data = "ves-io-723f7c87-b075-4c06-96e4-4e3cdedb4b97.ac.vh.ves.io"
+    data = "ves-io-a34d7e19-a421-4879-afa8-ac1055799bad.ac.vh.ves.io"
     ttl  = 3600
   }
 
   record {
     name = "_acme-challenge.apm"
     type = "CNAME"
-    data = "50115edde1d1446d8b5bd21a57973e45.autocerts.ves.volterra.io"
+    data = "c60067fcb06b401298a1de227c436c26.autocerts.ves.volterra.io"
     ttl  = 3600
   }
 
   record {
     name = "apm"
     type = "CNAME"
-    data = "ves-io-723f7c87-b075-4c06-96e4-4e3cdedb4b97.ac.vh.ves.io"
+    data = "ves-io-a34d7e19-a421-4879-afa8-ac1055799bad.ac.vh.ves.io"
     ttl  = 3600
   }
 
   record {
     name = "_acme-challenge.ingresslink"
     type = "CNAME"
-    data = "9fed3193e074427994483d999ef9e008.autocerts.ves.volterra.io"
+    data = "1d2f586b1fbf4b1b9507fcedd83278a7.autocerts.ves.volterra.io"
     ttl  = 3600
   }
 
   record {
     name = "ingresslink"
     type = "CNAME"
-    data = "ves-io-723f7c87-b075-4c06-96e4-4e3cdedb4b97.ac.vh.ves.io"
+    data = "ves-io-a34d7e19-a421-4879-afa8-ac1055799bad.ac.vh.ves.io"
     ttl  = 3600
   }
 
