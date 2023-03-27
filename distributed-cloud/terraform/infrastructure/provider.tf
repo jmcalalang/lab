f67085dd1c1100639bf3d6a61318b79f04eadde2
Jon Calalang <jon.calalang@gmail.com>
2023-03-27 09:39:15 -0700

    cert path wasnt updated

--- distributed-cloud/terraform/infrastructure/provider.tf
+++ distributed-cloud/terraform/infrastructure/provider.tf
@@ -15,6 +15,6 @@ terraform {
 # F5XC Provider
 
 provider "volterra" {
-  api_p12_file = "../../certs/f5-sa.console.ves.volterra.io.api-creds.p12"
+  api_p12_file = "../../../certs/f5-sa.console.ves.volterra.io.api-creds.p12"
   url          = "https://f5-sa.console.ves.volterra.io/api"
 }
