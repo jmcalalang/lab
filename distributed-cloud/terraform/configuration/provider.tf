f1d15971c969761a7669e586fee8faf259494a74
Jon Calalang <jon.calalang@gmail.com>
2023-04-03 21:36:28 -0700

    /kubeconfig/config

--- distributed-cloud/terraform/configuration/provider.tf
+++ distributed-cloud/terraform/configuration/provider.tf
@@ -28,7 +28,7 @@ provider "volterra" {
 }
 provider "kubernetes" {
   # Configuration options
-  config_path = "$PWD/kubeconfig/config"
+  config_path = "/kubeconfig/config"
 }
 provider "kubectl" {
   # Configuration options
