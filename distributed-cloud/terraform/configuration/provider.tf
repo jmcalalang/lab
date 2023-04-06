bdc939368874c8786245dbd65fc95fb07947d5d0
Jon Calalang <jon.calalang@gmail.com>
2023-04-06 09:15:22 -0700

    forgot a provider

--- distributed-cloud/terraform/configuration/provider.tf
+++ distributed-cloud/terraform/configuration/provider.tf
@@ -5,6 +5,10 @@ terraform {
     path = "distributed-cloud/terraform.tfstate"
   }
   required_providers {
+    volterra = {
+      source  = "volterraedge/volterra"
+      version = "0.11.19"
+    }
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = "2.19.0"
@@ -18,6 +22,10 @@ terraform {
 
 # F5XC Provider
 
+provider "volterra" {
+  api_p12_file = "../../../certs/f5-sa.console.ves.volterra.io.api-creds.p12"
+  url          = "https://f5-sa.console.ves.volterra.io/api"
+}
 provider "kubernetes" {
   host                   = var.ves_vk8s_host
   config_context         = var.ves_vk8s_context
