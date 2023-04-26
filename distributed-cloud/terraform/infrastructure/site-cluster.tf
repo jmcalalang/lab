fa6b5a45c5a71a47a2de8aa0b9da838caf95d77b
Jon Calalang <jon.calalang@gmail.com>
2023-04-26 13:37:50 -0500

    .name?

--- distributed-cloud/terraform/infrastructure/site-cluster.tf
+++ distributed-cloud/terraform/infrastructure/site-cluster.tf
@@ -104,7 +104,7 @@ resource "volterra_azure_vnet_site" "f5xc-azure-site" {
 }
 
 resource "volterra_tf_params_action" "f5xc-azure-site" {
-  site_name        = volterra_azure_vnet_site.f5xc-azure-site
+  site_name        = volterra_azure_vnet_site.f5xc-azure-site.name
   site_kind        = "azure_vnet_site"
   action           = "apply"
   wait_for_action  = true
