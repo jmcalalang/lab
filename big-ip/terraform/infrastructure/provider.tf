60d7fabf0df03a9702f42b918fb9276eaa6e7f08
Jon Calalang <jon.calalang@gmail.com>
2023-05-19 10:05:36 -0700

    provider fmt

--- big-ip/terraform/infrastructure/provider.tf
+++ big-ip/terraform/infrastructure/provider.tf
@@ -10,10 +10,10 @@ terraform {
     }
   }
   required_providers {
-#    aws = {
-#      source  = "hashicorp/aws"
-#      version = "4.65.0"
-#    }
+    #    aws = {
+    #      source  = "hashicorp/aws"
+    #      version = "4.65.0"
+    #    }
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "3.41.0"
