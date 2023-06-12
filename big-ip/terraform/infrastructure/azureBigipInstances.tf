88f24860835958c6315a88cadf36cda8af6603fe
Jon Calalang <jon.calalang@gmail.com>
2023-06-12 16:16:51 -0700

    cause why is it snakecase?

--- big-ip/terraform/infrastructure/azureBigipInstances.tf
+++ big-ip/terraform/infrastructure/azureBigipInstances.tf
@@ -185,7 +185,7 @@ resource "azurerm_network_security_group" "big-ip-management-sg" {
     priority                   = 100
     direction                  = "Inbound"
     access                     = "Allow"
-    protocol                   = "TCP"
+    protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "443"
     source_address_prefixes    = var.allowed_ips
@@ -197,7 +197,7 @@ resource "azurerm_network_security_group" "big-ip-management-sg" {
     priority                   = 105
     direction                  = "Inbound"
     access                     = "Allow"
-    protocol                   = "TCP"
+    protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "22"
     source_address_prefixes    = var.allowed_ips
@@ -209,7 +209,7 @@ resource "azurerm_network_security_group" "big-ip-management-sg" {
     priority                   = 110
     direction                  = "Inbound"
     access                     = "Allow"
-    protocol                   = "TCP"
+    protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "443"
     source_address_prefixes    = var.allowed_github_ips
@@ -239,7 +239,7 @@ resource "azurerm_network_security_group" "big-ip-external-sg" {
     priority                   = 100
     direction                  = "Inbound"
     access                     = "Allow"
-    protocol                   = "TCP"
+    protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "80"
     source_address_prefixes    = var.allowed_ips
@@ -251,7 +251,7 @@ resource "azurerm_network_security_group" "big-ip-external-sg" {
     priority                   = 105
     direction                  = "Inbound"
     access                     = "Allow"
-    protocol                   = "TCP"
+    protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "443"
     source_address_prefixes    = var.allowed_ips
