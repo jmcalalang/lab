c72391991291df69723303c4786d5ecc2773d803
Jon Calalang <jon.calalang@gmail.com>
2023-06-14 19:14:28 -0700

    prefix*

--- big-ip/terraform/infrastructure/azureBigipInstances.tf
+++ big-ip/terraform/infrastructure/azureBigipInstances.tf
@@ -171,7 +171,7 @@ resource "azurerm_network_security_group" "big-ip-management-sg" {
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "443"
-    source_address_prefixes    = "*"
+    source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
   security_rule {
@@ -182,7 +182,7 @@ resource "azurerm_network_security_group" "big-ip-management-sg" {
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "22"
-    source_address_prefixes    = "*"
+    source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
   tags = {
@@ -210,7 +210,7 @@ resource "azurerm_network_security_group" "big-ip-external-sg" {
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "443"
-    source_address_prefixes    = "*"
+    source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
   tags = {
