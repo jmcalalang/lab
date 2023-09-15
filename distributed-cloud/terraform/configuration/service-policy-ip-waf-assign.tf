cbf8e04ba95b7fcd7be3732584785d7b03751871
Jon Calalang <jon.calalang@gmail.com>
2023-09-15 16:19:18 -0700

    service policy?

---
+++ distributed-cloud/terraform/configuration/service-policy-ip-waf-assign.tf
@@ -0,0 +1,34 @@
+# Service Policy for WAF monitoring
+
+resource "volterra_service_policy_rule" "service_policy_rule_waf_monitoring" {
+  name      = "service-policy-rule-waf-monitoring"
+  namespace = var.namespace
+  labels = {
+    owner         = var.label-owner
+    resource-type = var.label-resource-type
+    environment   = var.label-environment
+  }
+
+  // One of the arguments from this list "any_asn asn_list asn_matcher" must be set
+  any_asn          = true
+  challenge_action = ["challenge_action"]
+
+  // One of the arguments from this list "any_client client_name ip_threat_category_list client_selector client_name_matcher" must be set
+
+  client_name_matcher {
+    exact_values = ["['new york', 'london', 'sydney', 'tokyo', 'cairo']"]
+
+    regex_values = ["['^new .*$', 'san f.*', '.* del .*']"]
+  }
+
+  // One of the arguments from this list "any_ip ip_prefix_list ip_matcher" must be set
+
+  ip_prefix_list {
+    invert_match = false
+    ip_prefixes  = ["192.168.20.0/24"]
+  }
+  waf_action {
+    // One of the arguments from this list "none waf_skip_processing waf_in_monitoring_mode app_firewall_detection_control data_guard_control" must be set
+    waf_in_monitoring_mode = true
+  }
+}
