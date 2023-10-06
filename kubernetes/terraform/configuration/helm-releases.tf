f1206321e28df334ea58b9c096b585565f6c3b8f
Jon Calalang <jon.calalang@gmail.com>
2023-10-05 20:56:15 -0700

    single ine?

--- kubernetes/terraform/configuration/helm-releases.tf
+++ kubernetes/terraform/configuration/helm-releases.tf
@@ -186,14 +186,8 @@ resource "helm_release" "nginx-plus-ingress" {
   }
 
   set {
-    name = "controller.globalConfiguration.spec"
-    value = {
-      "listeners" : [{
-        "name" : "tcp-listener",
-        "port" : "8888",
-        "protocol" : "TCP"
-      }]
-    }
+    name  = "controller.globalConfiguration.spec"
+    value = { "listeners" : [{ "name" : "tcp-listener", "port" : "8888", "protocol" : "TCP" }] }
   }
 
   set {
