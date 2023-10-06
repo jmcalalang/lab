b12c11e588ab1822eb8f07d5f198454f0a5b539b
Jon Calalang <jon.calalang@gmail.com>
2023-10-06 12:20:02 -0700

    skip listeners?

--- kubernetes/terraform/configuration/helm-releases.tf
+++ kubernetes/terraform/configuration/helm-releases.tf
@@ -187,8 +187,8 @@ resource "helm_release" "nginx-plus-ingress" {
 
   set {
     name  = "controller.globalConfiguration.spec"
-    value = "{\n  \"listeners\": [\n    {\n      \"name\": \"8080-listener\",\n      \"port\": 8080,\n      \"protocol\": \"TCP\"\n    },\n    {\n      \"name\": \"4433-listener\",\n      \"port\": 4433,\n      \"protocol\": \"TCP\"\n    }\n  ]\n}"
-  }
+    value = "[\n  {\n    \"name\": \"8080-listener\",\n    \"port\": 8080,\n    \"protocol\": \"TCP\"\n  },\n  {\n    \"name\": \"4433-listener\",\n    \"port\": 4433,\n    \"protocol\": \"TCP\"\n  }\n]"
+  }  
 
   set {
     name  = "controller.enableSnippets"
