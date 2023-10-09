129c8d68002cf4211f3a07d47791fafb42bd732f
Jon Calalang <jon.calalang@gmail.com>
2023-10-09 12:49:40 -0700

    fast fmt

--- big-ip/terraform/configuration/fastApplications.tf
+++ big-ip/terraform/configuration/fastApplications.tf
@@ -2,7 +2,7 @@
 
 # LDAP Application
 resource "bigip_fast_application" "ldap-10-0-2-5-fast" {
-  for_each        = fileset(path.module, "applications/fast/ldap/**.tpl")
-  fast_json       = templatefile("${path.module}/${each.key}", { monitor_passphrase = var.ad_service_ldap_password })
+  for_each  = fileset(path.module, "applications/fast/ldap/**.tpl")
+  fast_json = templatefile("${path.module}/${each.key}", { monitor_passphrase = var.ad_service_ldap_password })
   # template  = "bigip-fast-templates/ldap"
 }
