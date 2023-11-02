6097839ca66b1547f00016646849fca43d7e7d99
Jon Calalang <jon.calalang@gmail.com>
2023-11-02 11:34:59 -0700

    need vars to loop for fast

--- big-ip/terraform/configuration/fastApplications.tf
+++ big-ip/terraform/configuration/fastApplications.tf
@@ -3,7 +3,7 @@
 # HTTP Application
 resource "bigip_fast_application" "http-manifest" {
   for_each  = fileset(path.module, "applications/fast/http/**.tpl")
-  fast_json = templatefile("${path.module}/${each.key}")
+  fast_json = templatefile("${path.module}/${each.key}", { monitor_passphrase = var.ad_service_ldap_password })
   template  = "bigip-fast-templates/http"
 }
 
