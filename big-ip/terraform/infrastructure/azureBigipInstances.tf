cbfa0c2784f0fb6ce27b5ceba4a24d74e7ea0a58
Jon Calalang <jon.calalang@gmail.com>
2024-01-11 08:47:40 -0800

    tolist

--- big-ip/terraform/infrastructure/azureBigipInstances.tf
+++ big-ip/terraform/infrastructure/azureBigipInstances.tf
@@ -171,7 +171,7 @@ resource "azurerm_network_security_group" "big-ip-management-sg" {
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "22"
-    source_address_prefixes    = list([var.allowed_ips])
+    source_address_prefixes    = tolist([var.allowed_ips])
     destination_address_prefix = "*"
   }
   security_rule {
@@ -182,7 +182,7 @@ resource "azurerm_network_security_group" "big-ip-management-sg" {
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "443"
-    source_address_prefixes    = list([var.allowed_ips])
+    source_address_prefixes    = tolist([var.allowed_ips])
     destination_address_prefix = "*"
   }
   security_rule {
@@ -221,7 +221,7 @@ resource "azurerm_network_security_group" "big-ip-external-sg" {
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "443"
-    source_address_prefixes    = list([var.allowed_ips])
+    source_address_prefixes    = tolist([var.allowed_ips])
     destination_address_prefix = "*"
   }
   tags = {
