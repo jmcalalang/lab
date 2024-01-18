5d0e8a4ed4ecb7e99de4c9494ef1a39f0d69c686
Jon Calalang <jon.calalang@gmail.com>
2024-01-17 20:44:02 -0800

    adding extension scripts

--- services/active-directory/terraform/infrastructure/activeDirectoryInstances.tf
+++ services/active-directory/terraform/infrastructure/activeDirectoryInstances.tf
@@ -93,6 +93,23 @@ resource "azurerm_windows_virtual_machine" "active-directory-instance" {
   }
 }
 
+# Install IIS web server to the virtual machine
+resource "azurerm_virtual_machine_extension" "ad-domain-services" {
+  name                       = "ad-extension-${random_string.active-directory-random-string[0].result}"
+  virtual_machine_id         = azurerm_windows_virtual_machine.active-directory-instance[count.index].id
+  publisher                  = "Microsoft.Compute"
+  type                       = "CustomScriptExtension"
+  type_handler_version       = "2.0"
+  auto_upgrade_minor_version = true
+  count                      = sum([var.active-directory-instance-count])
+
+  settings = <<SETTINGS
+    {
+      "commandToExecute": "powershell -ExecutionPolicy Unrestricted Install-WindowsFeature -name AD-Domain-Services -IncludeManagementTools"
+    }
+  SETTINGS
+}
+
 ## Availability Set
 resource "azurerm_availability_set" "active-directory-instance" {
   name                = "a-set-${random_string.active-directory-random-string[0].result}"
