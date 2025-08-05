eec9acfc86e8a4643178cf2fd1e6e38de25d6ed2
Jon Calalang <jon.calalang@gmail.com>
2025-08-05 00:06:34 -0700

    string?

--- kubernetes/terraform/configuration/helm-releases.tf
+++ kubernetes/terraform/configuration/helm-releases.tf
@@ -214,13 +214,8 @@ resource "helm_release" "nginx-plus-ingress" {
   }
 
   set {
-    name = "controller.globalConfiguration.spec.listeners.name.tcp-listener"
-    value = [
-      jsonencode([
-        { name = "tcp-port", value = "8888" },
-        { name = "protocol", value = "TCP" }
-      ])
-    ]
+    name  = "controller.globalConfiguration.spec.listeners.name.tcp-listener"
+    value = "[\n    {\n        \"name\": \"tcp-listener\",\n        \"port\": 8888,\n        \"protocol\": \"TCP\"\n    }\n]"
   }
 
   set {
