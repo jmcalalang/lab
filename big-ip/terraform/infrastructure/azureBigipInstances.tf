9e2d72f75c0b18f0b0b4de82bdf1907488d9c8e8
Jon Calalang <jon.calalang@gmail.com>
2025-09-15 15:28:04 -0700

    true

--- big-ip/terraform/infrastructure/azureBigipInstances.tf
+++ big-ip/terraform/infrastructure/azureBigipInstances.tf
@@ -269,7 +269,7 @@ resource "azurerm_linux_virtual_machine" "big-ip-instance" {
   availability_set_id             = azurerm_availability_set.big-ip-instance.id
   count                           = sum([var.big-ip-instance-count])
   admin_username                  = var.big-ip-username
-  disable_password_authentication = false
+  disable_password_authentication = true
   computer_name                   = "big-ip-${random_uuid.big-ip-random-uuid[0].result}-${count.index}"
   custom_data = base64encode(templatefile("${path.module}/files/azure-bootstrap-big-ip-instances.tpl", {
     package_url    = var.bigip_runtime_init_package_url
