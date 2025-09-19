5499561a3231f7344a0a09d2d769201bb564c369
Jon Calalang <jon.calalang@gmail.com>
2025-09-19 08:42:01 -0700

    back to azurerm_virtual_machine

--- big-ip/terraform/infrastructure/azureBigipInstances.tf
+++ big-ip/terraform/infrastructure/azureBigipInstances.tf
@@ -256,40 +256,51 @@ resource "random_uuid" "big-ip-random-uuid" {
   count = sum([var.big-ip-instance-count])
 }
 
-resource "azurerm_linux_virtual_machine" "big-ip-instance" {
-  name                = "big-ip-${random_uuid.big-ip-random-uuid[0].result}-${count.index}"
-  location            = azurerm_resource_group.big-ip-resource-group.location
-  resource_group_name = azurerm_resource_group.big-ip-resource-group.name
+resource "azurerm_virtual_machine" "big-ip-instance" {
+  name                         = "big-ip-${random_uuid.big-ip-random-uuid[0].result}-${count.index}"
+  location                     = azurerm_resource_group.big-ip-resource-group.location
+  resource_group_name          = azurerm_resource_group.big-ip-resource-group.name
+  primary_network_interface_id = azurerm_network_interface.nic-management[count.index].id
   network_interface_ids = [
     azurerm_network_interface.nic-management[count.index].id,
     azurerm_network_interface.nic-external[count.index].id,
     azurerm_network_interface.nic-internal[count.index].id
   ]
-  size                            = var.big-ip-instance-size
-  availability_set_id             = azurerm_availability_set.big-ip-instance.id
-  count                           = sum([var.big-ip-instance-count])
-  admin_username                  = var.big-ip-username
-  admin_password                  = var.big-ip-password
-  disable_password_authentication = false
-  computer_name                   = "big-ip-${random_uuid.big-ip-random-uuid[0].result}-${count.index}"
-  custom_data = base64encode(templatefile("${path.module}/files/azure-bootstrap-big-ip-instances.tpl", {
-    package_url    = var.bigip_runtime_init_package_url
-    admin_username = var.big-ip-username
-  }))
+  vm_size                          = var.big-ip-instance-size
+  delete_data_disks_on_termination = true
+  delete_os_disk_on_termination    = true
+  availability_set_id              = azurerm_availability_set.big-ip-instance.id
+  count                            = sum([var.big-ip-instance-count])
+
+  # az vm image list -p f5-networks --all -f f5-big-ip-best -s 1g-best-hourly
   plan {
     publisher = "f5-networks"
     product   = var.big-ip-instance-offer
     name      = var.big-ip-instance-sku
   }
-  source_image_reference {
+  storage_image_reference {
     publisher = "f5-networks"
     offer     = var.big-ip-instance-offer
     sku       = var.big-ip-instance-sku
     version   = var.big-ip-version
   }
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "StandardSSD_LRS"
+  storage_os_disk {
+    name              = "os-disk-${random_uuid.big-ip-random-uuid[0].result}-${count.index}"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "StandardSSD_LRS"
+  }
+  os_profile {
+    computer_name  = "big-ip-${random_uuid.big-ip-random-uuid[0].result}-${count.index}"
+    admin_username = var.big-ip-username
+    admin_password = var.big-ip-password
+    custom_data = base64encode(templatefile("${path.module}/files/azure-bootstrap-big-ip-instances.tpl", {
+      package_url    = var.bigip_runtime_init_package_url
+      admin_username = var.big-ip-username
+    }))
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
   }
   identity {
     type = "SystemAssigned"
@@ -343,7 +354,7 @@ resource "azurerm_role_definition" "bigip-role-definition" {
 }
 
 resource "azurerm_role_assignment" "bigip-role-assignment-principal-id" {
-  principal_id                     = azurerm_linux_virtual_machine.big-ip-instance[count.index].identity[0].principal_id
+  principal_id                     = azurerm_virtual_machine.big-ip-instance[count.index].identity[0].principal_id
   role_definition_name             = azurerm_role_definition.bigip-role-definition.name
   scope                            = data.azurerm_subscription.primary.id
   skip_service_principal_aad_check = true
@@ -352,7 +363,7 @@ resource "azurerm_role_assignment" "bigip-role-assignment-principal-id" {
 
 ## Shutdown Schedule
 resource "azurerm_dev_test_global_vm_shutdown_schedule" "instance-group-azure-instances" {
-  virtual_machine_id    = azurerm_linux_virtual_machine.big-ip-instance[count.index].id
+  virtual_machine_id    = azurerm_virtual_machine.big-ip-instance[count.index].id
   location              = var.azure_location
   enabled               = true
   daily_recurrence_time = "1900"
@@ -367,6 +378,6 @@ resource "azurerm_dev_test_global_vm_shutdown_schedule" "instance-group-azure-in
 
 ## Wait for BIG-IP
 resource "time_sleep" "azure_bigip_ready" {
-  depends_on      = [azurerm_linux_virtual_machine.big-ip-instance]
+  depends_on      = [azurerm_virtual_machine.big-ip-instance]
   create_duration = var.bigip_ready
 }
