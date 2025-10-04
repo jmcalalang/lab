cdeb082e4fc33a758192017f9c271fd8f6de380a
Jon Calalang <jon.calalang@gmail.com>
2025-10-04 15:25:51 -0700

    depends issue

--- distributed-cloud/terraform/infrastructure/virtualSite.tf
+++ distributed-cloud/terraform/infrastructure/virtualSite.tf
@@ -1,20 +1,21 @@
 resource "volterra_known_label" "vsite_label" {
-  key       = volterra_known_label_key.vsite_key.key
-  namespace = "shared"
-  value     = "${var.label-owner}-azure-vsite-label"
+  key        = volterra_known_label_key.vsite_key.key
+  namespace  = "shared"
+  value      = "${var.label-owner}-azure-vsite-label"
+  depends_on = [volterra_known_label_key.vsite_key]
 }
 resource "volterra_known_label_key" "vsite_key" {
-  key        = "${var.label-owner}-azure-vsite-key"
-  namespace  = "shared"
-  depends_on = [volterra_known_label.vsite_label]
+  key       = "${var.label-owner}-azure-vsite-key"
+  namespace = "shared"
 }
 resource "volterra_virtual_site" "azure_vsite" {
-  name      = "${var.label-owner}azure-vsite"
-  namespace = "shared"
+  name       = "${var.label-owner}azure-vsite"
+  namespace  = "shared"
+  site_type  = "CUSTOMER_EDGE"
+  depends_on = [volterra_known_label.vsite_label, volterra_known_label_key.vsite_key]
   site_selector {
     expressions = [
       "${volterra_known_label_key.vsite_key.key} == ${volterra_known_label.vsite_label.value}"
     ]
   }
-  site_type = "CUSTOMER_EDGE"
 }
